0f82cf35e806cb4293d50e396da531df05fe651b
nitish-prajapati-zignuts <nitishp@zignuts.com>
2026-08-17 13:23:34 +0530

    feat: integrate automated versioning pipeline using semantic-release and GitHub Actions

---
+++ release.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: "npm version ${nextRelease.version} --prefix Frontend --no-git-tag-version && npm version ${nextRelease.version} --prefix Backend --no-git-tag-version"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "package.json",
+          "CHANGELOG.md",
+          "Frontend/package.json",
+          "Backend/package.json"
+        ],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github"
+  ]
+};
